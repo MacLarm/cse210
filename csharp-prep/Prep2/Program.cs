7f0a68563fdf5486fd05c8237fb33a42d9af5bf7
Mckay Larman <mckaylarman@Mckays-MacBook-Pro.local>
2023-09-14 20:04:51 -0600

    completed the prep 2 code. No stretch challenge

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade: ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+        string letterGrade = "";
+
+        if (grade >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (grade >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (grade >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (grade >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"You earned a {letterGrade}.");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better Luck next time");
+        }
     }
 }
