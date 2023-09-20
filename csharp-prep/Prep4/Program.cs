6101f56fd119d3bf40cd074e8b415c1b6a68efe2
Mckay Larman <mckaylarman@Mckays-MacBook-Pro.local>
2023-09-20 11:48:19 -0600

    checking the system

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.Write("Please enter as many numbers as you would like, when you are done, enter '0'. ");
+        List<int> Numbers = new List<int>();
+        int userInput;
+        do {
+            userInput = int.Parse(Console.ReadLine());
+            Numbers.Add(userInput);
+
+        } while (userInput != 0);
+
+        Console.WriteLine(Numbers);
     }
 }
