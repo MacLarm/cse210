f80035b32d83206a0af580e04a731879976f93e1
McKay <l.larman6@gmail.com>
2023-09-27 15:31:25 -0600

    fixing the paths

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        number = SquareNumber(number);
+        DisplayResult(name,number);
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int userNumber = int.Parse(Console.ReadLine());
+        return userNumber;
+    }   
+
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+    static void DisplayResult(string name, int number)
+    {
+        Console.WriteLine($"{name}, the square of your number is {number}");
     }
 }
