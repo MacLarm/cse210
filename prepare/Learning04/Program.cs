4d595acb31b690fe38edeca57aa9405fe2c5f484
McKay <l.larman6@gmail.com>
2023-10-27 16:23:07 -0600

    Completed Learning04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment sam = new Assignment("Sam","Multiplication");
+        string summary = sam.GetSummary();
+        Console.WriteLine(summary);
+
+        MathAssignment rod = new MathAssignment("Rod", "Fractions", "7.3", "8-19");
+        summary = rod.GetSummary();
+        string homework = rod.GetHomeworkList();
+        Console.WriteLine(summary);
+        Console.WriteLine(homework);
+
+        WritingAssignment matt = new WritingAssignment("matt", "US History", "Gettysburg");
+        summary = matt.GetSummary();
+        homework = matt.GetWritingInformation();
+        Console.WriteLine(summary);
+        Console.WriteLine(homework);
     }
 }
