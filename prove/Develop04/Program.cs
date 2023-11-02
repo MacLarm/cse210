ec246ebb63b3fb7398e83054bfb21d784e96be09
McKay <l.larman6@gmail.com>
2023-11-02 12:04:12 -0600

    started the mindfulness project

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop04 World!");
+        Breathing test = new Breathing(30);
+        test.display();
+        test.DisplayTime();
     }
+
+
 }
